02d8e173ccf98851c9b8c8e52292192bf0eed88c
Alexander Reif <reifialex@gmail.com>
2025-11-12 11:20:37 +0100

    Set database to auto_vacuum=FULL

--- server/src/core/database.ts
+++ server/src/core/database.ts
@@ -66,6 +66,7 @@ export async function initDatabase() {
 
     const em = _orm.em.fork()
     await em.execute('PRAGMA journal_mode=WAL;')
+    await em.execute('PRAGMA auto_vacuum=FULL;')
 
     _initObservers.forEach((initObserver) => {
         initObserver()
